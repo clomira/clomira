efdd480dab66b7085a616c90780317f1ba491fea
Sahib573 <gurdeepsingh74108@gmail.com>
2022-01-14 15:24:41 +0530

    ErrorPage and NavbarComp modified

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,19 +1,36 @@
 import Link from "next/link";
-import React from 'react';
 
 function NavBar() {
+  // return (
+  //   <div className="bg-slate-50 grid grid-cols-2">
+  //     <div className="pl-32 p-3">
+  //       <img src="/clomira_logo.jpg" alt="Clomira Logo" className="h-20 w-44" />
+  //     </div>
+  //     <div className="flex flex-row justify-center items-center">
+  //       <Link href="/"><div className="mr-20 cursor-pointer" >Home</div></Link>
+  //       <Link href="/login"><div className="mr-20 cursor-pointer" >Login</div></Link>
+  //       <Link href="/signup"><div className="mr-20 cursor-pointer" >Signup</div></Link>
+  //     </div>
+  //   </div>
+  // );
   return (
-    <div className="bg-slate-50 grid grid-cols-2">
-      <div className="pl-32 p-3">
-        <img src="/clomira_logo.jpg" alt="Clomira Logo" className="h-20 w-44" />
+    <nav className="bg-white px-8 pt-2 shadow-md">
+      <div className="-mb-px flex justify-center">
+        <a className="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/">
+          Home
+        </a>
+        <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/login">
+          Log In
+        </a>
+        <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/signup">
+          Sign Up
+        </a>
+        <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3" href="/study_material">
+          Study Material
+        </a>
       </div>
-      <div className="flex flex-row justify-center items-center">
-       <Link href="/"><div className="mr-20 cursor-pointer" >Home</div></Link>
-        <Link href="/login"><div className="mr-20 cursor-pointer" >Login</div></Link>
-        <Link href="/signup"><div className="mr-20 cursor-pointer" >Signup</div></Link>
-      </div>
-    </div>
-  );
+    </nav>
+  )
 }
 
 export default NavBar;
--- pages/404.tsx
+++ pages/404.tsx
@@ -1,49 +1,30 @@
 import { useEffect } from "react";
 import { ReactNode } from "react";
 import { useRouter } from "next/router";
-import styles from "../styles/err.module.css";
+import Image from "next/image";
+// import styles from "../styles/err.module.css";
 
 const err = () => {
     const rout = useRouter();
     useEffect(() => {
         setTimeout(() => {
             rout.push("/");
-        }, 3500)
+        }, 10000)
     }, []);
 
     return (
-        < div >
-            <div >
-                <header className={styles.topheader}>
-                </header>
-                <div>
-                    <div className={styles.starsec}></div>
-                    <div className={styles.starthird}></div>
-                    <div className={styles.starfourth}></div>
-                    <div className={styles.starfifth}></div>
-                </div>
-                <div className={styles.lamp__wrap}>
-                    <div className={styles.lamp}>
-                        <div className={styles.cable}></div>
-                        <div className={styles.cover}></div>
-                        <div className={styles.incover}>
-                            <div className="bulb"></div>
-                        </div>
-                        <div className={styles.light}></div>
+        <>
+            <section className="py-8 px-4 text-center">
+                <div className="max-w-auto md:max-w-lg mx-auto">
+                    <Image className="mb-8" src="/error.svg" height="500" width="500"alt="" />
+                    <h2 className="text-5xl mb-2 font-heading">Page not found</h2>
+                    <p className="mb-6 text-gray-500">We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or take a look at our.</p>
+                    <div>
+                        <a href="/"><button className="h-10 px-5 m-2 text-indigo-100 transition-colors duration-150 bg-indigo-700 rounded-lg focus:shadow-outline hover:bg-indigo-800">Back to Home</button></a>
                     </div>
                 </div>
-                <section className={styles.error}>
-                    <div className={styles.error__content}>
-                        <div className={`${styles.error__message} ${styles.message}`}></div>
-                        <h1 className={styles.message__title}>Page Not Found</h1>
-                        <p className={styles.message__text}>We're sorry, the page you were looking for isn't found here. The link you followed may either be broken or no longer exists. Please try again, or take a look at our.</p>
-                    </div>
-                    <div className={`${styles.error__nav} ${styles.enav}`}>
-                        <a href="/" className={styles.enav__link}></a>
-                    </div>
-                </section>
-            </div >
-        </ div>
+            </section>
+        </>
     )
 }
 
