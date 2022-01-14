09ad76829df40030fddcb5cd89304089444eac61
Sahib573 <gurdeepsingh74108@gmail.com>
2022-01-14 18:01:20 +0530

    modified Navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,35 +1,65 @@
 import Link from "next/link";
 
 function NavBar() {
-  // return (
-  //   <div className="bg-slate-50 grid grid-cols-2">
-  //     <div className="pl-32 p-3">
-  //       <img src="/clomira_logo.jpg" alt="Clomira Logo" className="h-20 w-44" />
-  //     </div>
-  //     <div className="flex flex-row justify-center items-center">
-  //       <Link href="/"><div className="mr-20 cursor-pointer" >Home</div></Link>
-  //       <Link href="/login"><div className="mr-20 cursor-pointer" >Login</div></Link>
-  //       <Link href="/signup"><div className="mr-20 cursor-pointer" >Signup</div></Link>
-  //     </div>
-  //   </div>
-  // );
+  //   return (
+  //     <nav classNameName="bg-white px-8 pt-2 shadow-md">
+  //       <div classNameName="-mb-px ">
+
+  //        <div classNameName="inline-block"><img src="/clomira_logo.jpg" alt="Clomira Logo" classNameName=" left-0 h-15 w-44 no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" /></div> 
+  //       <div classNameName="-mb-px top-0 flex justify-center">
+  //         <a classNameName="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/">
+  //           Home
+  //         </a>
+  //         <a classNameName="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/login">
+  //           Log In
+  //         </a>
+  //         <a classNameName="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/signup">
+  //           Sign Up
+  //         </a>
+  //         <a classNameName="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3" href="/study_material">
+  //           Study Material
+  //         </a>
+  //       </div>
+  //       </div>
+  //     </nav>
+  //   )
   return (
-    <nav className="bg-white px-8 pt-2 shadow-md">
-      <div className="-mb-px flex justify-center">
-        <a className="no-underline text-teal-dark border-b-2 border-teal-dark uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/">
-          Home
-        </a>
-        <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/login">
-          Log In
-        </a>
-        <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3 mr-8" href="/signup">
-          Sign Up
-        </a>
-        <a className="no-underline text-grey-dark border-b-2 border-transparent uppercase tracking-wide font-bold text-xs py-3" href="/study_material">
-          Study Material
-        </a>
+    <div className="sticky top-0 z-50">
+      <div className="bg-white shadow">
+        <div className="container mx-auto px-4">
+          <div className="flex items-center justify-between py-2">
+            <div>
+              <img src="/clomira_logo.jpg" alt="Clomira Logo" className="h-10 w-30" />
+            </div>
+
+            <div className="hidden sm:flex sm:items-center">
+              <a href="/" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Home</a>
+              <a href="/Doubts" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Doubts</a>
+              <a href="/study_material" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">Study Material</a>
+              <a href="/" className="text-gray-800 text-sm font-semibold hover:text-purple-600">College Guide</a>
+            </div>
+
+            <div className="hidden sm:flex sm:items-center">
+              <a href="/login" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">LogIn</a>
+              <a href="/signup" className="text-gray-800 text-sm font-semibold border px-4 py-2 rounded-lg hover:text-purple-600 hover:border-purple-600">SignUp</a>
+            </div>
+          </div>
+
+          <div className="block sm:hidden bg-white border-t-2 py-2">
+            <div className="flex flex-col">
+              <a href="/" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Products</a>
+              <a href="doubts" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Marketplace</a>
+              <a href="/study_material" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Partners</a>
+              <a href="/" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mb-1">Pricing</a>
+              <div className="flex justify-between items-center border-t-2 pt-2">
+                <a href="/login" className="text-gray-800 text-sm font-semibold hover:text-purple-600 mr-4">LogIn</a>
+                <a href="/signup" className="text-gray-800 text-sm font-semibold border px-4 py-1 rounded-lg hover:text-purple-600 hover:border-purple-600">SignUp</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </nav>
+    </div>
   )
 }
 
