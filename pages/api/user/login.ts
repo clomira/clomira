d1c9aba31c488c733ee685c2ef5f7bef619cdedb
Somesh Singh <someshsingh298@gmail.com>
2022-01-14 23:46:58 +0530

    added header

--- pages/api/user/login.ts
+++ pages/api/user/login.ts
@@ -7,7 +7,7 @@ export default async function Users(req:NextApiRequest, res:NextApiResponse) {
     if (req.method === 'POST') {
         const client = await clientPromise
         const db = client.db("clomira")
-        const data = await db.collection("users").find({user_email:req.body._userEmail,user_pass:req.body._userPass}).limit(10).toArray()
+        const data = await db.collection("users").find({user_email:req.body._userEmail,user_pass:req.body._userPass}).toArray()
         
         const response = JSON.parse(JSON.stringify(data))
         res.json(response)
